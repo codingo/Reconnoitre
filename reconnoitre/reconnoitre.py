d17cecf26b487ef814ac53b2ffbb0226f74ea436
Michael Skelton <michaelskelton@cancerqld.org.au>
2017-04-24 23:04:30 +1000

    Setup command line options

---
+++ reconnoitre/reconnoitre.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+from argparse import ArgumentParser
+import sys
+
+def print_banner():
+    print("  __")
+    print("|\"\"\"\-=  RECONNOITRE")
+    print("(____)      An OSCP scanner\n")
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("-t", dest="target_hosts", required=True, help="Set a target range of addresses to target. Ex 10.11.0.1-255" )
+    parser.add_argument("-o", dest="output_directory", help="Set the output directory.")
+    parser.add_argument("--quiet", dest="quiet",  action="store_true", help="Supress banner and headers to limit to comma dilimeted results only.", default=False)
+    arguments = parser.parse_args()
+
+    if len(sys.argv) == 1:
+        print_banner()
+        parser.error("No arguments given.")
+        parser.print_usage
+        sys.exit()
+
+    if arguments.quiet is not True:
+        print_banner()
+
+
+if __name__ == "__main__":
+    main()
