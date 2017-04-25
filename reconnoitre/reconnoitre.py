93a2f05de7557236611173ee26887408b4992a8d
Michael Skelton <michaelskelton@cancerqld.org.au>
2017-04-25 11:17:19 +1000

    Added ping sweep option

--- reconnoitre/reconnoitre.py
+++ reconnoitre/reconnoitre.py
@@ -12,6 +12,7 @@ def main():
     parser = ArgumentParser()
     parser.add_argument("-t", dest="target_hosts", required=True, help="Set a target range of addresses to target. Ex 10.11.1.1-255" )
     parser.add_argument("-o", dest="output_directory", required=True, help="Set the output directory.")
+    parser.add_argument("-pS", dest="ping_sweep", action="store_true", help="Perform a ping sweep and discover hosts.", default=False)
     parser.add_argument("--quiet", dest="quiet",  action="store_true", help="Supress banner and headers to limit to comma dilimeted results only.", default=False)
     arguments = parser.parse_args()
 
@@ -23,8 +24,8 @@ def main():
 
     if arguments.quiet is not True:
         print_banner()
-
-    ping_sweeper(arguments.target_hosts, arguments.output_directory, arguments.quiet)
+    if arguments.ping_sweep is True:
+        ping_sweeper(arguments.target_hosts, arguments.output_directory, arguments.quiet)
 
 if __name__ == "__main__":
     main()
