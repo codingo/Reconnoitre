cc863375ffbb8bf60602dc9b059f338bff3a8d06
Michael Skelton <michaelskelton@cancerqld.org.au>
2017-04-25 11:48:53 +1000

    Improved targetting for dns recon

--- reconnoitre/find_dns.py
+++ reconnoitre/find_dns.py
@@ -1,6 +1,14 @@
 import subprocess
 import sys
+import os
 from directory_helper import check_directory
 
 def find_dns(target_hosts, output_directory, quiet):
     check_directory(output_directory)
+    output_file = output_directory + "/DNS-Servers.txt"
+
+    if(os.path.isdir(target_hosts)):
+        targets = open(target_hosts, 'r')
+    else:
+        targets = open(output_directory + "/targets.txt")
+    print("[*] Loaded targets from: %s" % targets)
