ce4fc89eb3c7fc44b5afb343b9f1b3144d6a2f8a
Michael Skelton <michaelskelton@cancerqld.org.au>
2017-04-25 11:13:56 +1000

    Corrected command line params for ping-sweep

--- reconnoitre/reconnoitre.py
+++ reconnoitre/reconnoitre.py
@@ -10,8 +10,8 @@ def print_banner():
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument("-t", dest="target_hosts", required=True, help="Set a target range of addresses to target. Ex 10.11.0.1-255" )
-    parser.add_argument("-o", dest="output_directory", help="Set the output directory.")
+    parser.add_argument("-t", dest="target_hosts", required=True, help="Set a target range of addresses to target. Ex 10.11.1.1-255" )
+    parser.add_argument("-o", dest="output_directory", required=True, help="Set the output directory.")
     parser.add_argument("--quiet", dest="quiet",  action="store_true", help="Supress banner and headers to limit to comma dilimeted results only.", default=False)
     arguments = parser.parse_args()
 
