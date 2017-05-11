f44b06b0192f2c28b237381ed1e1d1cfad715eef
Michael Skelton <michaelskelton@cancerqld.org.au>
2017-05-11 16:25:56 +1000

    Fixed SNMP scan returning non-0 error

--- reconnoitre/snmp_walk.py
+++ reconnoitre/snmp_walk.py
@@ -61,5 +61,5 @@ def snmp_scans(ip_address, output_directory):
     print("[+] Performing SNMP scans for %s to %s" % (ip_address, output_directory))	
     print("   [>] Performing snmpwalk on public tree for: %s - Checking for System Processes" % (ip_address))
     SCAN = "snmpwalk -c public -v1 %s 1.3.6.1.2.1.25.1.6.0 > %s%s-systemprocesses.txt"  % (ip_address, output_directory, ip_address)
-    subprocess.check_output(SCAN, shell=True)
+    call(SCAN)
     print("[+] Completed SNMP scans for %s" % (ip_address))
