0138a549a69bcf2701e9e15acc796476df410e9c
Michael Skelton <michaelskelton@cancerqld.org.au>
2017-05-11 16:29:05 +1000

    Fixed SNMP Public tree walk

--- reconnoitre/snmp_walk.py
+++ reconnoitre/snmp_walk.py
@@ -1,5 +1,4 @@
 import subprocess
-from subprocess import call
 import multiprocessing
 import socket
 import os
@@ -60,7 +59,9 @@ def snmp_walk(target_hosts, output_directory, quiet):
 
 def snmp_scans(ip_address, output_directory):
     print("[+] Performing SNMP scans for %s to %s" % (ip_address, output_directory))	
+
     print("   [>] Performing snmpwalk on public tree for: %s - Checking for System Processes" % (ip_address))
     SCAN = "snmpwalk -c public -v1 %s 1.3.6.1.2.1.25.1.6.0 > %s%s-systemprocesses.txt"  % (ip_address, output_directory, ip_address)
-    call(SCAN)
+    subprocess.run(SCAN)
+
     print("[+] Completed SNMP scans for %s" % (ip_address))
