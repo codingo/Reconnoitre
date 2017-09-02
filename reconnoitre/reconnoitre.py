6f297cdd605ec865e8b44da703a422cb641ec522
Michael Skelton <michaelskelton@cancerqld.org.au>
2017-09-02 20:09:33 +1000

    Wiireframing for virtual host scanning. Command line flags and class template.

--- reconnoitre/reconnoitre.py
+++ reconnoitre/reconnoitre.py
@@ -6,6 +6,7 @@ from find_dns import find_dns
 from service_scan import service_scan
 from hostname_scan import hostname_scan
 from snmp_walk import snmp_walk
+from virtual_host_scanning import virtual_hosts
 
 
 def print_banner():
@@ -25,6 +26,7 @@ def main():
     parser.add_argument("--hostnames",  dest="hostname_scan", action="store_true", help="Attempt to discover target hostnames and write to 0-name.txt and hostnames.txt.", default=False)
     parser.add_argument("--snmp",       dest="perform_snmp_walk", action="store_true", help="Perform service scan over targets.", default=False)
     parser.add_argument("--quick",      dest="quick",   action="store_true", required=False, help="Move to the next target after performing a quick scan and writing first-round recommendations.", default=False)    
+    parser.add_argument("--virtualhosts",      dest="virtualhosts", action="store_true", help="Scan for virtual host networks using the asscoatied wordlist.", default=False)
     parser.add_argument("--quiet",      dest="quiet",   action="store_true", help="Supress banner and headers to limit to comma dilimeted results only.", default=False)
     parser.add_argument("--exec",       dest="follow",  action="store_true", help="Execute shell comamnds from recommendations as they are discovered. Likely to lead to very long execute times depending on the wordlist being used.", default=False)
     parser.add_argument("--simple_exec",dest="quickfollow",  action="store_true", help="Execute non-brute forcing shell comamnds only as they are discovered.", default=False)
@@ -43,24 +45,36 @@ def main():
 
     if arguments.quiet is not True:
         print_banner()
+
     if arguments.ping_sweep is True:
         print("[#] Performing ping sweep")
         ping_sweeper(arguments.target_hosts, arguments.output_directory, arguments.quiet)
+
     if arguments.hostname_scan is True:
         print("[#] Identifying hostnames")
         hostname_scan(arguments.target_hosts, arguments.output_directory, arguments.quiet)
+
     if arguments.find_dns_servers is True:
         print("[#] Identifying DNS Servers")
         find_dns(arguments.target_hosts, arguments.output_directory, arguments.quiet)
+
     if arguments.perform_service_scan is True:
         print("[#] Performing service scans")
         if arguments.find_dns_servers is True:
             service_scan(arguments.target_hosts, arguments.output_directory, arguments.find_dns_servers, arguments.quiet, arguments.quick)
         else:
             service_scan(arguments.target_hosts, arguments.output_directory, '', arguments.quiet, arguments.quick)
+ 
+    if arguments.virtualhosts is True:
+        if arguments.wordlist is False:
+            print("[#] Unable to scan for virtual hosts as no wordlist was provided.")
+        else:
+            vh = virtual_hosts()
+
     if arguments.perform_snmp_walk is True:
         print("[#] Performing SNMP walks")
         snmp_walk(arguments.target_hosts, arguments.output_directory, arguments.quiet)
+
 
         
 if __name__ == "__main__":
