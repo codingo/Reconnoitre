5ae64c4e4b3b917d65b1985c0348ec3b94f8e67f
Michael Skelton <michaelskelton@cancerqld.org.au>
2017-09-03 14:10:32 +1000

    Fixed arguments bug from code port

--- reconnoitre/virtual_host_scanner.py
+++ reconnoitre/virtual_host_scanner.py
@@ -12,12 +12,12 @@ class virtual_host_scanner(object):
         output: folder to write output file to
 
     """
-
+     
     def __init__(self, target, output, port=80, ignore_http_codes='404', ignore_content_length=0, wordlist="./wordlist/virtual-host-scanning.txt"):
         self.target = target
         self.output = output + '/' + target + '_virtualhosts.txt'
         self.port = port
-        self.ignore_http_codes = list(map(int, args.ignore_http_codes.replace(' ', '').split(',')))
+        self.ignore_http_codes = list(map(int, ignore_http_codes.replace(' ', '').split(',')))
         self.ignore_content_length = ignore_content_length
         self.wordlist = wordlist
 
