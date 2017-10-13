6498b0be6125c5c3f67dbcfcca3c13f822d6596f
esjotka <w.pitera@gmail.com>
2017-10-13 23:47:37 +0100

    wrap folder in all commands with quotes in case there are spaces

--- reconnoitre/file_helper.py
+++ reconnoitre/file_helper.py
@@ -59,20 +59,20 @@ def write_recommendations(results, ip_address, outputdir):
        ports = []
        line = line.strip()
        if ("tcp" in line) and ("open" in line) and not ("Discovered" in line):
-           while "  " in line: 
+           while "  " in line:
                line = line.replace("  ", " ");
            service = line.split(" ")[2]
            port = line.split(" ")[0]
 
            if service in serv_dict:
-               ports = serv_dict[service] 
+               ports = serv_dict[service]
 
-           ports.append(port) 
+           ports.append(port)
            serv_dict[service] = ports
-   
+
    print("[+] Writing findings for %s" % (ip_address))
    f = open(recommendations_file, 'w')
-   for serv in serv_dict: 
+   for serv in serv_dict:
        ports = serv_dict[serv]
        if ("ftp" in serv):
            for port in ports:
@@ -81,7 +81,7 @@ def write_recommendations(results, ip_address, outputdir):
                print("   [>] Found FTP service on %s:%s" % (ip_address, port))
                f.write("   [>] Use nmap scripts for further enumeration or hydra for password attack, e.g\n")
                f.write("      [=] nmap -sV -Pn -vv -p%s --script=ftp-anon,ftp-bounce,ftp-libopie,ftp-proftpd-backdoor,ftp-vsftpd-backdoor,ftp-vuln-cve2010-4221 -oN '%s/%s_ftp.nmap' -oX '%s/%s_ftp_nmap_scan_import.xml' %s\n" % (port, outputdir, ip_address, outputdir, ip_address, ip_address))
-               f.write("      [=] hydra -L User List -P Pass List -f -o %s/%s_ftphydra.txt -u %s -s %s ftp\n" % (outputdir, ip_address, ip_address, port))	
+               f.write("      [=] hydra -L User List -P Pass List -f -o %s/%s_ftphydra.txt -u %s -s %s ftp\n" % (outputdir, ip_address, ip_address, port))
        elif (serv == "http") or (serv == "ssl/http") or ("https" in serv) or ("http" in serv):
            for port in ports:
                port = port.split("/")[0]
@@ -90,11 +90,11 @@ def write_recommendations(results, ip_address, outputdir):
                f.write("   [>] Use nikto & dirb / dirbuster for service enumeration, e.g\n")
                f.write("      [=] nikto -h %s -p %s > %s/%s_nikto.txt\n" % (ip_address, port, outputdir, ip_address))
                f.write("      [=] dirb http://%s:%s/ -o %s/%s_dirb.txt\n" % (ip_address, port, outputdir, ip_address))
-               f.write("      [=] gobuster -w /usr/share/wordlists/SecLists/Discovery/Web_Content/common.txt -u http://%s:%s/ -s '200,204,301,302,307,403,500' -e > %s/%s_gobuster_common.txt -t 50 \n" % (ip_address, port, outputdir, ip_address))
-               f.write("      [=] gobuster -w /usr/share/wordlists/SecLists/Discovery/Web_Content/cgis.txt -u http://%s:%s/ -s '200,204,301,307,403,500' -e > %s/%s_gobuster_cgis.txt -t 50 \n" % (ip_address, port, outputdir, ip_address))
+               f.write("      [=] gobuster -w /usr/share/wordlists/SecLists/Discovery/Web_Content/common.txt -u http://%s:%s/ -s '200,204,301,302,307,403,500' -e > '%s/%s_gobuster_common.txt' -t 50 \n" % (ip_address, port, outputdir, ip_address))
+               f.write("      [=] gobuster -w /usr/share/wordlists/SecLists/Discovery/Web_Content/cgis.txt -u http://%s:%s/ -s '200,204,301,307,403,500' -e > '%s/%s_gobuster_cgis.txt' -t 50 \n" % (ip_address, port, outputdir, ip_address))
                f.write("   [>] Use curl and W3M (apt install w3m) to retreive web headers and find host information, e.g\n")
                f.write("      [=] curl -i %s\n" % (ip_address))
-               f.write("      [=] w3m -dump %s/robots.txt  > %s/%s_robots.txt\n" % (ip_address, outputdir, ip_address))
+               f.write("      [=] w3m -dump %s/robots.txt  > '%s/%s_robots.txt'\n" % (ip_address, outputdir, ip_address))
        elif "mysql" in serv:
            for port in ports:
                port = port.split("/")[0]
@@ -108,14 +108,14 @@ def write_recommendations(results, ip_address, outputdir):
                print("   [>] Found telnet service on %s:%s" % (ip_address, port))
                f.write("   [>] Check out the server headers to enumerate further\n")
                f.write("      [=] nc -nv %s %s\n" % (ip_address, port))
-       elif "microsoft-ds" in serv:	
+       elif "microsoft-ds" in serv:
            for port in ports:
                port = port.split("/")[0]
                f.write("[*] Found MS SMB service on %s:%s\n" % (ip_address, port))
                print("   [>] Found MS SMB service on %s:%s" % (ip_address, port))
                f.write("   [>] Use nmap scripts or enum4linux for further enumeration, e.g\n")
                f.write("      [=] nmap -sV -Pn -vv -p 139,%s --script=smb-vuln* --script-args=unsafe=1 -oN '%s/%s_smb.nmap' -oX '%s/%s_smb_nmap_scan_import.xml' %s\n" % (port, outputdir, ip_address, outputdir, ip_address, ip_address))
-               f.write("      [=] enum4linux %s > %s/%s_enum4linux.txt\n" % (ip_address, outputdir, ip_address))
+               f.write("      [=] enum4linux %s > '%s/%s_enum4linux.txt'\n" % (ip_address, outputdir, ip_address))
                f.write("      [=] nmap -sV -Pn -vv -p %s --script=smb-enum-users -oN '%s/%s_smb_smb-enum-users.nmap' %s\n" % (port, outputdir, ip_address, ip_address))
        elif "ms-sql" in serv:
            for port in ports:
@@ -123,7 +123,7 @@ def write_recommendations(results, ip_address, outputdir):
                f.write("[*] Found MS SQL service on %s:%s\n" % (ip_address, port))
                print("   [>] Found MS SQL service on %s:%s" % (ip_address, port))
                f.write("   [>] Use nmap scripts for further enumeration, e.g\n")
-               f.write("      [=] nmap -vv -sV -Pn -p %s --script=ms-sql-info,ms-sql-config,ms-sql-dump-hashes --script-args=mssql.instance-port=%s,smsql.username-sa,mssql.password-sa -oX %s/%s_mssql_nmap_scan_import.xml %s" % (port, port, outputdir, ip_address, ip_address))
+               f.write("      [=] nmap -vv -sV -Pn -p %s --script=ms-sql-info,ms-sql-config,ms-sql-dump-hashes --script-args=mssql.instance-port=%s,smsql.username-sa,mssql.password-sa -oX '%s/%s_mssql_nmap_scan_import.xml' %s" % (port, port, outputdir, ip_address, ip_address))
        elif ("msdrdp" in serv) or ("ms-wbt-server" in serv):
            for port in ports:
                port = port.split("/")[0]
@@ -146,7 +146,7 @@ def write_recommendations(results, ip_address, outputdir):
                f.write("   [>] Use nmap scripts, onesixtyone or snmwalk for further enumeration, e.g\n")
                f.write("      [=] nmap -sV -Pn -vv -p%s --script=snmp-netstat,snmp-processes -oN '%s/%s_snmp.nmap' -oX '%s/%s_snmp_nmap_scan_import.xml' %s\n" % (port, outputdir, ip_address, outputdir, ip_address, ip_address))
                f.write("      [=] onesixtyone %s\n" % (ip_address))
-               f.write("      [=] snmpwalk -c public -v1 %s > %s/%s_snmpwalk.txt\n" % (ip_address, outputdir, ip_address))
+               f.write("      [=] snmpwalk -c public -v1 %s > '%s/%s_snmpwalk.txt'\n" % (ip_address, outputdir, ip_address))
        elif "ssh" in serv:
            for port in ports:
                port = port.split("/")[0]
@@ -157,5 +157,5 @@ def write_recommendations(results, ip_address, outputdir):
                f.write("      [=] hydra -f -V -t 1 -l root -P rockyou.txt -s %s %s ssh\n" % (port, ip_address))
                f.write("   [>] Use nmap to automate banner grabbing and key fingerprints, e.g.\n")
                f.write("      [=] nmap %s -p %s -sV --script=ssh-hostkey -oN '%s/%s_ssh-hostkey.nmap' \n" % (ip_address, port, outputdir, ip_address))
-               
-   f.close()     
+
+   f.close()
--- reconnoitre/service_scan.py
+++ reconnoitre/service_scan.py
@@ -2,7 +2,7 @@ import subprocess
 import multiprocessing
 import socket
 import os
-import time 
+import time
 from multiprocessing import Process, Queue
 from file_helper import check_directory
 from file_helper import load_targets
@@ -12,11 +12,11 @@ from file_helper import write_recommendations
 
 def nmap_scan(ip_address, output_directory, dns_server, quick):
    ip_address = ip_address.strip()
-   
+
    print("[+] Starting quick nmap scan for %s" % (ip_address))
    QUICKSCAN = "nmap -sC -sV %s -oA '%s/%s.quick'"  % (ip_address, output_directory, ip_address)
    quickresults = subprocess.check_output(QUICKSCAN, shell=True)
-   
+
    write_recommendations(quickresults, ip_address, output_directory)
    print("[*] TCP quick scans completed for %s" % ip_address)
 
@@ -31,7 +31,7 @@ def nmap_scan(ip_address, output_directory, dns_server, quick):
    else:
        print("[+] Starting detailed TCP/UDP nmap scans for %s" % (ip_address))
        TCPSCAN = "nmap -vv -Pn -sS -A -sC -p- -T 3 -script-args=unsafe=1 -n %s -oN '%s/%s.nmap' -oX '%s/%s_nmap_scan_import.xml' %s"  % (dns_server, output_directory, ip_address, output_directory, ip_address, ip_address)
-       UDPSCAN = "nmap -sC -sV -sU %s -oA %s/%s-udp" % (ip_address, output_directory, ip_address)
+       UDPSCAN = "nmap -sC -sV -sU %s -oA '%s/%s-udp'" % (ip_address, output_directory, ip_address)
 
    udpresults = subprocess.check_output(UDPSCAN, shell=True)
    tcpresults = subprocess.check_output(TCPSCAN, shell=True)
@@ -48,7 +48,7 @@ def valid_ip(address):
         return False
 
 
-def target_file(target_hosts, output_directory, dns_server, quiet, quick):    
+def target_file(target_hosts, output_directory, dns_server, quiet, quick):
     targets = load_targets(target_hosts, output_directory, quiet)
     target_file = open(targets, 'r')
     try:
@@ -63,22 +63,22 @@ def target_file(target_hosts, output_directory, dns_server, quiet, quick):
 
        host_directory = output_directory + "/" + ip_address
        nmap_directory = host_directory + "/scans"
-       
+
        jobs = []
        p = multiprocessing.Process(target=nmap_scan, args=(ip_address, nmap_directory, dns_server, quick))
        jobs.append(p)
        p.start()
-    target_file.close() 
+    target_file.close()
 
 
 def target_ip(target_hosts, output_directory, dns_server, quiet, quick):
     print("[*] Loaded single target: %s" % target_hosts)
-    target_hosts = target_hosts.strip()    
+    target_hosts = target_hosts.strip()
     create_dir_structure(target_hosts, output_directory)
-    
+
     host_directory = output_directory + "/" + target_hosts
     nmap_directory = host_directory + "/scans"
-    
+
     jobs = []
     p = multiprocessing.Process(target=nmap_scan, args=(target_hosts, nmap_directory, dns_server, quick))
     jobs.append(p)
