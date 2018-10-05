99a9083f2fd51052a982457242445a96498f0081
Szabó Péter <1254135+szabopeter@users.noreply.github.com>
2018-10-05 22:49:32 +0200

    Split up ping_sweeper method

--- reconnoitre/ping_sweeper.py
+++ reconnoitre/ping_sweeper.py
@@ -8,22 +8,24 @@ def ping_sweeper(target_hosts, output_directory, quiet):
     check_directory(output_directory)
     output_file = output_directory + "/targets.txt"
 
-    print("[+] Writing discovered targets to: %s" % output_file)
-    f = open(output_file, 'w')
-
     print("[+] Performing ping sweep over %s" % target_hosts)
 
+    lines = call_nmap_sweep(target_hosts)
+    live_hosts = parse_nmap_output_for_live_hosts(lines)
+    write_live_hosts_list_to_file(output_file, live_hosts)
+
+    for ip_address in live_hosts:
+        print("   [>] Discovered host: %s" % (ip_address))
+
+    print("[*] Found %s live hosts" % (len(live_hosts)))
+    print("[*] Created target list %s" % (output_file))
+
+
+def call_nmap_sweep(target_hosts):
     SWEEP = "nmap -n -sP %s" % (target_hosts)
     results = subprocess.check_output(SWEEP, shell=True)
     lines = str(results, "utf-8").split("\n")
-    
-    live_hosts = parse_nmap_output_for_live_hosts(lines)
-    f.write("\n".join(live_hosts))
-    for ip_address in live_hosts:
-        print("   [>] Discovered host: %s" % (ip_address))
-    print("[*] Found %s live hosts" % (len(live_hosts)))
-    print("[*] Created target list %s" % (output_file))
-    f.close()
+    return lines
 
 
 def parse_nmap_output_for_live_hosts(lines):
@@ -35,4 +37,10 @@ def parse_nmap_output_for_live_hosts(lines):
                   if "Nmap scan report for" in line]
 
     return live_hosts
+
+
+def write_live_hosts_list_to_file(output_file, live_hosts):
+    print("[+] Writing discovered targets to: %s" % output_file)
+    with open(output_file, 'w') as f:
+        f.write("\n".join(live_hosts))
 
