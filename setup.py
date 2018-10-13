0305a24424753024a5eb7be9eeb63ced7f3ce7cc
CaptainFreak <patelshoeb4@gmail.com>
2018-10-13 10:33:40 +0530

    Fixed code style

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ with open("README.md") as file:
         license="GPLv3",
         description="A reconnaissance tool made for the OSCP labs to automate information gathering, "
                     "and service enumeration whilst creating a directory structure to store results,"
-                    "findings and exploits used for each host, recommended commands to execute and directory structures for storing loot and flags.",
+                    "findings and exploits used for each host, recommended commands to execute "
+                    "and directory structures for storing loot and flags.",
         long_description=file.read(),
         author="codingo",
         version=__version__,
