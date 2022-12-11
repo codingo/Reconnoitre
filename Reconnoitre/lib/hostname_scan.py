08b726ece52cfa726b422f821d9abec2a23ec154
mahajanhrishikesh <mahajanhm4@gmail.com>
2022-12-11 11:32:53 -0500

    Refactored and Tested

--- Reconnoitre/lib/hostname_scan.py
+++ Reconnoitre/lib/hostname_scan.py
@@ -1,48 +1,51 @@
 import os
+from lib.utility import Utility
 
-from Reconnoitre.lib.file_helper import check_directory
-from Reconnoitre.lib.subprocess_helper import run_scan
+class HostNameScan():
+    def __init__(self, target_hosts, output_directory, quiet):
+        self.target_hosts = target_hosts
+        self.output_directory = output_directory
+        self.quiet = quiet
+        self.hostnames = 0
+        self.output_file = "{}/hostnames.txt".format(self.output_directory)
 
+    def hostname_scan(self, target_hosts, output_directory, quiet):
+        Utility.check_directory(output_directory)
+        f = open(self.output_file, 'w')
+        print("[+] Writing hostnames to: %s" % self.output_file)
 
-def hostname_scan(target_hosts, output_directory, quiet):
-    check_directory(output_directory)
-    output_file = output_directory + "/hostnames.txt"
-    f = open(output_file, 'w')
-    print("[+] Writing hostnames to: %s" % output_file)
+        SWEEP = ''
 
-    hostnames = 0
-    SWEEP = ''
+        if (os.path.isfile(target_hosts)):
+            SWEEP = "nbtscan -q -f %s" % (target_hosts)
+        else:
+            SWEEP = "nbtscan -q %s" % (target_hosts)
 
-    if (os.path.isfile(target_hosts)):
-        SWEEP = "nbtscan -q -f %s" % (target_hosts)
-    else:
-        SWEEP = "nbtscan -q %s" % (target_hosts)
+        results = Utility.run_scan(SWEEP)
+        lines = results.split("\n")
 
-    results = run_scan(SWEEP)
-    lines = results.split("\n")
+        for line in lines:
+            line = line.strip()
+            line = line.rstrip()
 
-    for line in lines:
-        line = line.strip()
-        line = line.rstrip()
+            # Final line is blank which causes list index issues if we don't
+            # continue past it.
+            if " " not in line:
+                continue
 
-        # Final line is blank which causes list index issues if we don't
-        # continue past it.
-        if " " not in line:
-            continue
+            while "  " in line:
+                line = line.replace("  ", " ")
 
-        while "  " in line:
-            line = line.replace("  ", " ")
+            ip_address = line.split(" ")[0]
+            host = line.split(" ")[1]
 
-        ip_address = line.split(" ")[0]
-        host = line.split(" ")[1]
+            if (self.hostnames > 0):
+                f.write('\n')
 
-        if (hostnames > 0):
-            f.write('\n')
+            print("   [>] Discovered hostname: %s (%s)" % (host, ip_address))
+            f.write("%s - %s" % (host, ip_address))
+            self.hostnames += 1
 
-        print("   [>] Discovered hostname: %s (%s)" % (host, ip_address))
-        f.write("%s - %s" % (host, ip_address))
-        hostnames += 1
-
-    print("[*] Found %s hostnames." % (hostnames))
-    print("[*] Created hostname list %s" % (output_file))
-    f.close()
+        print("[*] Found %s hostnames." % (self.hostnames))
+        print("[*] Created hostname list %s" % (self.output_file))
+        f.close()
